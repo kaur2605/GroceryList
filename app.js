8a41b2e7c76ea92bcf8f5a85399db0d7c0248556
Gurdeep Kaur <32565739+kaur2605@users.noreply.github.com>
2020-01-24 22:44:26 +0100

    New changes
    createitem function added

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const additemaction = document.querySelector(".add-items");
+const AddItemAction = document.querySelector(".add-items");
 const input = document.querySelector(".input-item");
 const submit = document.querySelector(".submit");
 
-const list = document.querySelector(".Grocery-items");
+const list = document.querySelector(".Grocery-list");
 const displayaction = document.querySelector(".display-items");
 const clear = document.querySelector(".clear");
 
@@ -13,31 +13,35 @@ function addlist(event) {
   let value = input.value;
 
   if (value === "") {
-    showaction(additemaction, "please add item first", false);
+    showaction(AddItemAction, "please add item first", false);
   } else {
-    showaction(additemaction, `${value} is added to the list`, true);
+    showaction(AddItemAction, `${value} is added to the list`, true);
+    createitem(value);
   }
 }
 
 function showaction(element, text, value) {
   if (value === true) {
-    console.log(value);
-
-    element.classlist.add("success");
+    element.classList.add("success");
     element.innerText = text;
     input.value = "";
     setTimeout(function() {
-      element.classlist.remove("success");
+      element.classList.remove("success");
     }, 3000);
   } else {
-    console.log(value);
-
-    element.classlist.add("alert");
-
+    element.classList.add("alert");
     element.innerText = text;
     input.value = "";
     setTimeout(function() {
-      element.classlist.remove("alert");
+      element.classList.remove("alert");
     }, 3000);
   }
+}
+
+function createitem(value) {
+  const parent = document.createElement("div");
+  parent.classList.add("Grocery-items");
+  parent.innerHTML = `<h3 class="items">${value}</h3>
+        <a href="#" class="Grocery-items-link"> <i class="far fa-trash-alt"></i></a>`;
+  list.appendChild(parent);
 }
